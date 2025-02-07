da49ff9df1c31b2139d75aa6aa8e9f4ec69ec92d
Eyal Keysar <Gamma-user59@greendreamteam.onmicrosoft.com>
2025-02-07 11:09:23 +0200

    router testing

--- Backend/app/api/v1/authentication_router.py
+++ Backend/app/api/v1/authentication_router.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Response, HTTPException, status
 from app.services.authentication_service import AuthenticationService
-from app.schemas.user_schema import UserSignup
 from datetime import timedelta
+from app.schemas.user_schema import UserSignup
 
 router = APIRouter()
 
@@ -10,22 +10,23 @@ def signup(user_data: UserSignup, response: Response):
     """
     Handles user signup and sets a session cookie.
     """
-    user, token = AuthenticationService.signup_user(user_data)
+    print(user_data)
+    # user, token = AuthenticationService.signup_user(user_data)
 
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="User could not be created"
-        )
+    # if not user:
+    #     raise HTTPException(
+    #         status_code=status.HTTP_400_BAD_REQUEST,
+    #         detail="User could not be created"
+    #     )
 
     # Set cookie (token will expire in 7 days)
     response.set_cookie(
         key="session_token",
-        value=token,
+        value="testtoken12",
         httponly=True,  # Prevent JavaScript access
         max_age=int(timedelta(days=7).total_seconds()),
         samesite="Lax",  # Protect against CSRF
         secure=True  # Send only over HTTPS
     )
 
-    return {"message": "User created successfully", "user": user}
+    return {"message": "User created successfully", "user": "testuser"}
--- Backend/app/services/group_management_service.py
+++ Backend/app/services/group_management_service.py
@@ -7,6 +7,5 @@ class GroupManagementService:
     def __init__(self, group_repository: GroupRepositoryInterface):
         self.group_repository = group_repository
 
-
     def create_group(self, group: Group, user: User):
         return self.group_repository.create_group(group, user)
