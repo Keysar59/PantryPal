297715ed3ac59f88ae910ef3a7f1c780cac83bd3
l55 <gammauserl55@gmail.com>
2025-02-11 14:27:29 +0200

    entities implementation

--- Backend/app/domain/entities/product.py
+++ Backend/app/domain/entities/product.py
@@ -2,4 +2,23 @@ class Product:
     product_id: str
     product_name: str
     product_description: str
+
+    def __init__(self, product_id: str, product_name: str, product_description: str):
+        self.product_id = product_id
+        self.product_name = product_name
+        self.product_description = product_description
+
+    def get_product_id(self):
+        return self.product_id
+
+    def get_product_name(self):
+        return self.product_name
+
+    def get_product_description(self):
+        return self.product_description
+
+    
+    
+    
+    
     
