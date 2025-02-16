b0a6284342895e42ba08552b70a4ee61b9b1a68a
Eyal Keysar <Gamma-user59@greendreamteam.onmicrosoft.com>
2025-02-16 10:24:40 +0200

    added testing router and service

--- Backend/app/infrastructures/dependency_injection.py
+++ Backend/app/infrastructures/dependency_injection.py
@@ -1,7 +1,10 @@
 
 from app.services.authentication_service import AuthenticationService
-
+from app.services.testing_service import TestingService
 from app.infrastructures.repositories.user_repository_txt import UserRepositoryTxt
 
 def get_authentication_service() -> AuthenticationService:
     return AuthenticationService(UserRepositoryTxt())
+
+def get_testing_service() -> TestingService:
+    return TestingService()
