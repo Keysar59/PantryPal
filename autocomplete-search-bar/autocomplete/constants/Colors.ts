3eab4e8e06bacc76ee614f6b18aca12980770aff
M-G-P-48 <Gamma-user48@greendreamteam.onmicrosoft.com>
2025-03-18 18:10:15 +0200

    basic search bar

---
+++ autocomplete-search-bar/autocomplete/constants/Colors.ts
@@ -0,0 +1,19 @@
+const tintColorLight = "#FF5A5F";
+const tintColorDark = "#FF5A5F";
+
+export default {
+  light: {
+    text: "#000",
+    background: "#fff",
+    tint: tintColorLight,
+    tabIconDefault: "#ccc",
+    tabIconSelected: tintColorLight,
+  },
+  dark: {
+    text: "#fff",
+    background: "#000",
+    tint: tintColorDark,
+    tabIconDefault: "#ccc",
+    tabIconSelected: tintColorDark,
+  },
+};
---
+++ autocomplete-search-bar/autocomplete/types/product.ts
@@ -0,0 +1,5 @@
+export type Product = {
+    barcode: string;
+    name: string;
+    image_url: string | null;
+};
